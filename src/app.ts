97f6a9a8d90095c5d6443f8faed7b24a9380b3be
Benny Reznik <BennyR@codevalue.net>
2019-07-09 11:05:23 +0300

    template engines

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import path from "path";
 import express from "express";
 import cors from "cors";
-import { config } from "./routes";
+import { config, clients } from "./routes";
 import expressWinston from "express-winston";
+import exphbs from "express-handlebars";
 import logError from "./middleware/logError";
 import { createExpressWinstonOptions } from "./utils/logger";
 import { initPassport } from "./utils/passport";
@@ -17,6 +18,20 @@ app.use(express.urlencoded({ extended: true }));
 
 // app.use(expressWinston.logger(createExpressWinstonOptions()));
 app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+    helpers: {
+      increment: (v: number) => v + 1
+    }
+  })
+);
+app.set("view engine", "handlebars");
+
+clients.forEach(o => app.use(`/app${o.prefix}`, o.router));
 
 Object.keys(config).forEach(k => {
   const routeConfig = config[k];
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,20 @@
 import { router as productsRoute } from "./productsRoute";
 import { router as categoriesRoute } from "./categoriesRoute";
 import { router as loginRoute } from "./loginRoute";
+import { router as clientapp } from "./clientapp";
 import { Router } from "express";
 
 interface RouteConfig {
   prefix: string;
   router: Router;
 }
+
+export const clients = [
+  {
+    prefix: "/",
+    router: clientapp
+  }
+];
 
 const config: { [k: string]: RouteConfig } = {
   products: {
